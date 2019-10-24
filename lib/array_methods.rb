69c25847e03ebd849cf9f974733f91d1312399d2
Sadia Badhon <sadia.islam.badhon@gmail.com>
2019-10-24 18:31:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,8 +10,15 @@ end
 
 
 def find_max_value(array)
-  # Add your solution here
+   max = 0
+    array.length.times do |count|
+      if array[count] > max
+        max = array[count]
+      end
+    end
+  max
 end
+
 
 def find_min_value(array)
   # Add your solution here
